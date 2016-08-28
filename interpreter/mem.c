fbfdf0446903300d1b8b7ba7013d32c1f3d49c39
Camil Staps <info@camilstaps.nl>
2016-08-28 21:45:49 +0200

    Added my_realloc

--- interpreter/mem.c
+++ interpreter/mem.c
@@ -11,6 +11,15 @@ void* my_calloc(size_t num, size_t size) {
 	return ptr;
 }
 
+void* my_realloc(void* ptr, size_t size) {
+	ptr = realloc(ptr, size);
+	if (!ptr) {
+		perror(NULL);
+		exit(EXIT_FAILURE);
+	}
+	return ptr;
+}
+
 void my_free(void* ptr) {
 	free(ptr);
 }
--- interpreter/mem.h
+++ interpreter/mem.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 void* my_calloc(size_t num, size_t size);
+void* my_realloc(void* ptr, size_t size);
 void my_free(void* ptr);
 
 #endif
